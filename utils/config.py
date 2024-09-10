1b904af2dedb51773c2fc6e77ee23ab0c8b1028e
ReganovaLisa <reganova.ev@phystech.edu>
2024-09-10 11:09:58 +0200

    add utils

---
+++ utils/config.py
@@ -0,0 +1,7 @@
+API_KEY = "ENTER-YOUR-API-KEY"
+def assistant(content: str):
+    return { "role": "assistant", "content": content }
+def user(content: str):
+    return { "role": "user", "content": content }
+def system(content: str):
+    return { "role": "system", "content": content }
